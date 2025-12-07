e8cbe561701442624b6a7b32adde94bb2bb66d7d
Fateme Aslani <f.aslani2361@gmail.com>
2025-12-07 04:53:08 +0330

    Create a login, menu, and banner

--- src/app/+pages/+public/login-page/login-page.ts
+++ src/app/+pages/+public/login-page/login-page.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, Inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
 export class LoginPage {
 
+  username:string='';
+  pass:string='';
+  remember:boolean=false;
+  message:string='';
+  router=inject(Router)
+
+  test (){
+    if (this.username=='admin' && this.pass == 'admin') {
+      this.router.navigateByUrl('/private/dashboard')
+    } else {
+      this.message='نام کاربری یا کلمه عبور اشتباه است '
+    }
+  }
 }
